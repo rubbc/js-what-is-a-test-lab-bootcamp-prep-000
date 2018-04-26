f58c666f21c942bf217052c3477812328ce066d2
ruben cattan <rubencattan@gmail.com>
2018-04-26 12:29:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // write your code below!
+<<<<<<< HEAD
 var name = "Susan"
+=======
+vr name = "Susan"
+>>>>>>> 955fa597b1c7feaec1441014d9bd9193f4de6adc
 var height = 32
 var message = `${name} is ${height} inches tall`
 
